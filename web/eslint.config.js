7a5bb1b75a27632fa6850157386b3590309dc858
Javier Rivero Iglesias <jriveroiglesias@al.uloyola.es>
2026-07-24 16:34:33 +0200

    Add ESLint configuration for TypeScript and React

---
+++ web/eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist", "node_modules"] },
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      jsxA11y.flatConfigs.recommended
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.browser
+    }
+  }
+);
